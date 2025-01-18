4054dab3e916b29d5d87d40385bdac1eb2f7ff2f
Ioannis Ligkas <Ioannis_Ligkas@sluz.ch>
2025-01-18 20:41:07 +0100

    added CSS

--- shoppinglist/src/views/HomeView.vue
+++ shoppinglist/src/views/HomeView.vue
@@ -1,8 +1,28 @@
 <script setup>
-    import { RouterView, RouterLink } from 'vue-router'  
+import { RouterLink } from 'vue-router';
 </script>
+
 <template>
-    <RouterLink to="/lists">
-        Temp
+  <div class="home">
+    <img src="../assets/bilder/shoppingcart.png" alt="Shopping Cart" class="shopping-cart" />
+    
+    <div class="home-content">
+      <h1>Einkaufsbuddy</h1>
+      <p>Your online shopping list</p>
+      
+      <div class="button-container">
+        <RouterLink to="/lists" class="home-button">
+          List Overview
+        </RouterLink>
+        <RouterLink to="/search" class="home-button">
+          Search for food
+        </RouterLink>
+      </div>
+    </div>
+
+    <!-- Login-Button -->
+    <RouterLink to="/login" class="home-button login-button">
+      Login
     </RouterLink>
+  </div>
 </template>
